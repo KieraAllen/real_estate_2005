3db2fbe84095434043fdfa382e4fcd2718fed9e4
Kiera Allen <kieraaallen@gmail.com>
2020-05-13 09:39:35 -0600

    Test above_market_average method

--- test/house_test.rb
+++ test/house_test.rb
@@ -34,4 +34,45 @@ class HouseTest < Minitest::Test
 
     assert_equal [room_1, room_2], house.rooms
   end
+
+  def test_it_is_not_above_market_average_if_price_is_under_500000
+    house = House.new("$400000", "123 sugar lane")
+    house2 = House.new("$600000", "456 sugar lane")
+
+    assert_equal false, house.above_market_average?
+    assert_equal true, house2.above_market_average?
+  end
 end
+
+
+# pry(main)> room_1 = Room.new(:bedroom, 10, '13')
+# #=> #<Room:0x00007fccd29b5720...>
+#
+# pry(main)> room_2 = Room.new(:bedroom, 11, '15')
+# #=> #<Room:0x00007fccd2985f48...>
+#
+# pry(main)> room_3 = Room.new(:living_room, 25, '15')
+# #=> #<Room:0x00007fccd383c2d0...>
+#
+# pry(main)> room_4 = Room.new(:basement, 30, '41')
+# #=> #<Room:0x00007fccd297dc30...>
+#
+# pry(main)> house.add_room(room_1)
+#
+# pry(main)> house.add_room(room_2)
+#
+# pry(main)> house.add_room(room_3)
+#
+# pry(main)> house.add_room(room_4)
+#
+# pry(main)> house.rooms_from_category(:bedroom)
+# #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
+#
+# pry(main)> house.rooms_from_category(:basement)
+# #=> [#<Room:0x00007fccd297dc30...>]
+#
+# pry(main)> house.area
+# #=> 1900
+#
+# pry(main)> house.details
+# #=> {"price" => 400000, "address" => "123 sugar lane"}

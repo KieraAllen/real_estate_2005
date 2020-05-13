6863e346506cbe8de1d61a8e1cbdae053ed475da
Kiera Allen <kieraaallen@gmail.com>
2020-05-13 09:33:23 -0600

    Test it can add rooms and return array of rooms added

--- test/house_test.rb
+++ test/house_test.rb
@@ -22,20 +22,16 @@ class HouseTest < Minitest::Test
 
     assert_equal [], house.rooms
   end
+
+  def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+    house.add_room(room_1)
+    house.add_room(room_2)
+
+    assert_equal [room_1, room_2], house.rooms
+  end
 end
-
-
-
-
-# pry(main)> room_1 = Room.new(:bedroom, 10, '13')
-# #=> #<Room:0x00007fccd29b5720...>
-#
-# pry(main)> room_2 = Room.new(:bedroom, 11, '15')
-# #=> #<Room:0x00007fccd2985f48...>
-#
-# pry(main)> house.add_room(room_1)
-#
-# pry(main)> house.add_room(room_2)
-#
-# pry(main)> house.rooms
-# #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
